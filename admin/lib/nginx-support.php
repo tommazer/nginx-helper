1a808984c86186711c86622d8e104c76eef5881d
Tom Mazer <tom@likesocial.nl>
2016-09-07 15:06:54 +0200

    Change support tab

--- admin/lib/nginx-support.php
+++ admin/lib/nginx-support.php
@@ -5,20 +5,14 @@ namespace hoasted\WP\Nginx {
         <form id="support" action="" method="post" class="clearfix">
             <div class="postbox">
                 <h3 class="hndle">
-                    <span><?php _e( 'Support Forums', 'hoastedcache' ); ?></span>
+                    <span><?php _e( 'Support', 'hoastedcache' ); ?></span>
                 </h3>
                 <div class="inside">
                     <table class="form-table">
                         <tr valign="top">
-                            <th><?php _e( 'Free Support', 'hoastedcache' ); ?></th>
+                            <th><?php _e( 'Support', 'hoastedcache' ); ?></th>
                             <td>
-                                <a href="https://rtcamp.com/support/forum/wordpress-nginx/" title="<?php _e( 'Free Support Forum', 'hoastedcache' ); ?>" target="_blank"><?php _e( 'Link to forum', 'hoastedcache' ); ?></a>
-                            </td>
-                        </tr>
-                        <tr valign="top">
-                            <th><?php _e( 'Premium Support', 'hoastedcache' ); ?></th>
-                            <td>
-                                <a href="https://rtcamp.com/wordpress-nginx/pricing/" title="<?php _e( 'Premium Support Forum', 'hoastedcache' ); ?>" target="_blank"><?php _e( 'Link to forum', 'hoastedcache' ); ?></a>
+                                <a href="https://www.hoasted.com/contact" title="<?php _e( 'Contact us', 'hoastedcache' ); ?>" target="_blank"><?php _e( 'Contact us', 'hoastedcache' ); ?></a>
                             </td>
                         </tr>
                     </table>
