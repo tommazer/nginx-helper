35f2c53b04df0423d2e7de423d8f15c19b7844a7
Tom Mazer <tom@likesocial.nl>
2016-09-07 15:12:17 +0200

    Change sidebar

--- admin/lib/nginx-sidebar.php
+++ admin/lib/nginx-sidebar.php
@@ -15,52 +15,11 @@ namespace hoasted\WP\Nginx {
 				<span><?php _e( 'Need Help?', 'hoastedcache' ); ?></span>
 			</h3>
 			<div class="inside">
-				<p><?php printf( __( 'Please use our <a href="%s">free support forum</a>.', 'hoastedcache' ), 'http://rtcamp.com/support/forum/wordpress-nginx/' ); ?></p>
+				<p><?php printf( __( 'Please <a href="%s">contact us</a>.', 'hoastedcache' ), 'https://www.hoasted.com/contact' ); ?></p>
 			</div>
 		</div>
 
-		<div class="postbox" id="social">
-			<h3 class="hndle">
-				<span><?php _e( 'Getting Social is Good', 'hoastedcache' ); ?></span>
-			</h3>
-			<div style="text-align:center;" class="inside">
-				<a class="nginx-helper-facebook" title="<?php _e( 'Become a fan on Facebook', 'nginx-helper' ); ?>" target="_blank" href="http://www.facebook.com/rtCamp.solutions/"></a>
-				<a class="hoastedcache-twitter" title="<?php _e( 'Follow us on Twitter', 'hoastedcache' ); ?>" target="_blank" href="https://twitter.com/rtcamp/"></a>
-				<a class="nginx-helper-gplus" title="<?php _e( 'Add to Circle', 'hoastedcache' ); ?>" target="_blank" href="https://plus.google.com/110214156830549460974/posts"></a>
-				<a class="nginx-helper-rss" title="<?php _e( 'Subscribe to our feeds', 'hoastedcache' ); ?>" target="_blank" href="http://feeds.feedburner.com/rtcamp/"></a>
-			</div>
-		</div>
-
-		<div class="postbox" id="useful-links">
-			<h3 class="hndle">
-				<span><?php _e( 'Useful Links', 'hoastedcache' ); ?></span>
-			</h3>
-			<div class="inside">
-				<ul role="list">
-					<li role="listitem">
-						<a href="https://rtcamp.com/wordpress-nginx/" title="<?php _e( 'WordPress-Nginx Solutions', 'hoastedcache' ); ?>"><?php _e( 'WordPress-Nginx Solutions', 'hoastedcache' ); ?></a>
-					</li>
-					<li role="listitem">
-						<a href="https://rtcamp.com/services/wordPress-themes-design-development/" title="<?php _e( 'WordPress Theme Devleopment', 'hoastedcache' ); ?>"><?php _e( 'WordPress Theme Devleopment', 'hoastedcache' ); ?></a>
-					</li>
-					<li role="listitem">
-						<a href="http://rtcamp.com/services/wordpress-plugins/" title="<?php _e( 'WordPress Plugin Development', 'hoastedcache' ); ?>"><?php _e( 'WordPress Plugin Development', 'hoastedcache' ); ?></a>
-					</li>
-					<li role="listitem">
-						<a href="http://rtcamp.com/services/custom-wordpress-solutions/" title="<?php _e( 'WordPress Consultancy', 'hoastedcache' ); ?>"><?php _e( 'WordPress Consultancy', 'hoastedcache' ); ?></a>
-					</li>
-					<li role="listitem">
-						<a href="https://rtcamp.com/easyengine/" title="<?php _e( 'easyengine (ee)', 'hoastedcache' ); ?>"><?php _e( 'easyengine (ee)', 'hoastedcache' ); ?></a>
-					</li>        
-				</ul>
-			</div>
-		</div>
-
-		<div class="postbox" id="latest_news">
-			<div title="<?php _e( 'Click to toggle', 'hoastedcache' ); ?>" class="handlediv"><br /></div>
-			<h3 class="hndle"><span><?php _e( 'Latest News', 'hoastedcache' ); ?></span></h3>
-			<div class="inside"><img src ="<?php echo admin_url(); ?>/images/wpspin_light.gif" /><?php _e( 'Loading...', 'hoastedcache' ); ?></div>
-		</div><?php
+		<?php
 	}
 
 // End of default_admin_sidebar()
