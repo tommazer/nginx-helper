252b6f64d3ddf3b0beea1f84074958d36e6ca858
Tom Mazer <tom@likesocial.nl>
2016-09-07 15:01:42 +0200

    Remove Local server cache files

--- admin/lib/nginx-general.php
+++ admin/lib/nginx-general.php
@@ -151,12 +151,12 @@ namespace hoasted\WP\Nginx {
 												&nbsp;<?php _e( 'Using a GET request to <strong>PURGE/url</strong> (Default option)', 'hoastedcache' ); ?><br />
 												<small><?php _e( 'Uses the <strong><a href="https://github.com/FRiCKLE/ngx_cache_purge">ngx_cache_purge</a></strong> module. ', 'hoastedcache' ); ?></small>
 											</label><br />
-											<label for="purge_method_unlink_files">
-												<input type="radio" value="unlink_files" id="purge_method_unlink_files" name="purge_method"<?php checked( isset( $hc_wp_nginx_helper->options['purge_method'] ) ? $hc_wp_nginx_helper->options['purge_method'] : '', 'unlink_files' ); ?>>
-												&nbsp;<?php _e( 'Delete local server cache files', 'hoastedcache' ); ?><br />
-												<small><?php _e( 'Checks for matching cache file in <strong>hc_wp_nginx_HELPER_CACHE_PATH</strong>. Does not require any other modules. Requires that the cache be stored on the same server as WordPress. You must also be using the default nginx cache options (levels=1:2) and (fastcgi_cache_key "$scheme$request_method$host$request_uri"). ', 'hoastedcache' ); ?></small>
-
-											</label><br />
+<!--											<label for="purge_method_unlink_files">-->
+<!--												<input type="radio" value="unlink_files" id="purge_method_unlink_files" name="purge_method"--><?php //checked( isset( $hc_wp_nginx_helper->options['purge_method'] ) ? $hc_wp_nginx_helper->options['purge_method'] : '', 'unlink_files' ); ?><!-->-->
+<!--												&nbsp;--><?php //_e( 'Delete local server cache files', 'hoastedcache' ); ?><!--<br />-->
+<!--												<small>--><?php //_e( 'Checks for matching cache file in <strong>hc_wp_nginx_HELPER_CACHE_PATH</strong>. Does not require any other modules. Requires that the cache be stored on the same server as WordPress. You must also be using the default nginx cache options (levels=1:2) and (fastcgi_cache_key "$scheme$request_method$host$request_uri"). ', 'hoastedcache' ); ?><!--</small>-->
+<!---->
+<!--											</label><br />-->
 										</fieldset>
 									</td>
 								</tr>
